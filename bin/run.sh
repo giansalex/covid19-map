61278f40d49956b613f8a1bb43a99a78a1163e91
Giancarlos Salas <giansalex@gmail.com>
2020-04-10 19:46:02 -0500

    add publish ipfs sh script

---
+++ bin/run.sh
@@ -0,0 +1,36 @@
+echo "Start download COVID data"
+
+DATA_DIR="./data" node ./script/download-data.js
+
+x=$?
+
+if [ "$x" -eq "0" ]; then
+	echo "Building publish package"
+else
+	echo "No available data for now"
+	exit 0
+fi
+
+publishDir="./publish"
+npm run build
+mkdir -p $publishDir
+cp index.html $publishDir
+cp manifest.json $publishDir
+cp favicon.png $publishDir
+cp -r images $publishDir
+cp -r dist $publishDir
+
+echo "Upload publish to IPFS"
+
+ipfsHash=$(UPLOAD_DIR=$publishDir node script/upload-ipfs.js)
+
+x=$?
+
+if [ "$x" -eq "0" ]; then
+	echo "Updating DNSLink to $ipfsHash"
+else
+    echo "Error publishing to IPFS"
+	exit 0
+fi
+
+IPFS_HASH=$ipfsHash node ./script/update-dnslink.js
